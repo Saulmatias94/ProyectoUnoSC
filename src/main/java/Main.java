10aff868a31516da4e12f0886a699391733084c8
Saul Cifuentes 1 <saulmatias94@gmail.com>
2020-12-20 00:32:04 -0300

    Implementando Gasto

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,19 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Gasto> lista= new ArrayList<Gasto>();
+        ArrayList<Gasto> lista = new ArrayList<Gasto>();
 
-        lista.add(new Gasto("Luz",12000));
-        lista.add(new Gasto("Agua",7000));
-        lista.add(new Gasto("Internet",30000));
-        lista.add(new Gasto("Gas",20000));
+        lista.add(new Gasto("Luz", 12000, false));
+        lista.add(new Gasto("Agua", 7000, false));
+        lista.add(new Gasto("Internet", 30000, false));
+        lista.add(new Gasto("Gas", 20000,false));
 
-        double resultado=lista.stream().mapToDouble(gasto-gasto.getValor()*1.21)
-                .filter(gasto->gasto<20000)
-                .sum();
+        double resultado=lista.stream().mapToDouble(gasto->gasto.getValor()*1.21)
+         .filter(gasto->gasto<20000)
+         .sum();
 
-        System.out.println(resultado);
+         System.out.println(resultado);
 
     }
 }
